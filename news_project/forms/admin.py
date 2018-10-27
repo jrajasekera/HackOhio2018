90dec68d506fa7d3ec4d81157ee6cf513c7a912b
Vincent Diago <vdiago@alumni.stvm.com>
2018-10-27 13:56:05 -0400

    added tables

--- news_project/forms/admin.py
+++ news_project/forms/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Source
+from .models import Sort
+from .models import Category
+from .models import Language
+from .models import Country
 
 #Register models here
 
 admin.site.register(Source)
+
+admin.site.register(Sort)
+
+admin.site.register(Category)
+
+admin.site.register(Language)
+
+admin.site.register(Country)
