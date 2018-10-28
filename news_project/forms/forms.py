f0c4285c4fad4b558d445211f755302f2debdeef
Jude Rajasekera <juderajasekera@gmail.com>
2018-10-28 02:26:53 -0400

    Add More Forms to search app

--- news_project/forms/forms.py
+++ news_project/forms/forms.py
@@ -1,4 +1,58 @@
 from django import forms
+from .models import Source, SortBy, Category, Language, Country
+
+
+CHOICES = [("choice1","c1"),("choice2","c2")]
+#querySet = SortBy.objects.values('displaySortBy')
+# print(querySet)
+# len=len(list(querySet))
+# print(len)
+# print(list(querySet)[2]["displaySortBy"])
+
+
+
+def QuerySetToList(qSet, keyName):
+    newList = []
+    lenth = len(list(qSet))
+    for x in range (0,lenth):
+        newList.append(list(qSet)[x][keyName])
+    return newList
+
+def listToTupleList(list):
+    tupleList = []
+    length = len(list)
+    for x in range (0, length):
+        tuple = (list[x],list[x])
+        tupleList.append(tuple)
+    return tupleList
+
+def QuerySetToTupleList(qSet, keyName):
+    listItems = QuerySetToList(qSet, keyName)
+    return listToTupleList(listItems)
 
 class SearchForm(forms.Form):
     inputKeywords = forms.CharField(label='Topic', max_length=30)
+
+    #sortBy form
+    sortKey = 'displaySortBy'
+    sortQs = SortBy.objects.values(sortKey)
+    ddSortBy = forms.CharField(widget=forms.Select(choices=QuerySetToTupleList(sortQs, sortKey)))
+    
+    # Category
+    categoryKey = 'displayCat'
+    sortQs = Category.objects.values(categoryKey)
+    ddCategory = forms.CharField(widget=forms.Select(choices=QuerySetToTupleList(sortQs, categoryKey)))
+
+    # Language
+    languageKey = 'displayLang'
+    sortQs = Language.objects.values(languageKey)
+    ddLanguage = forms.CharField(widget=forms.Select(choices=QuerySetToTupleList(sortQs, languageKey)))
+
+    #Source USE EVERY SOURCE FOR NOW
+
+    #country form USE US FOR NOW
+    # countryKey = 'displayctry'
+    # sortQs = Country.objects.values(countryKey)
+    # countrySelect = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=QuerySetToTupleList(sortQs, countryKey))
+
+    
--- news_project/forms/views.py
+++ news_project/forms/views.py
@@ -6,14 +6,24 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
 from .models import Source, SortBy, Category, Language, Country
 
+def printFormItem(item,form):
+    print((item + ' = ' + form.cleaned_data[item]))
+
+def printForm(items, form):
+    length = len(items)
+    for x in range (0, length):
+        printFormItem(items[x],form)
+    return
+
 @ensure_csrf_cookie
 def searchPage(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
             print('VALID FORM. NAVIGATING TO RESULTS PAGE')
-            # do something with form.cleaned_data['my_form_field_name']            
-            print(form.cleaned_data['inputKeywords'])
+            #  form.cleaned_data['my_form_field_name']
+            formList = ["inputKeywords","ddSortBy", "ddCategory", "ddLanguage"]
+            printForm(formList, form)
             return HttpResponseRedirect('http://cnn.com')
         else:
             print('FORM NOT VALID, RETURNING TO SEARCH PAGE')
