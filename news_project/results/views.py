c262a0ee96ac5ddd4bcb64c46d449968d7878e6e
Jude Rajasekera <juderajasekera@gmail.com>
2018-10-28 08:50:05 -0400

    Add url creator to results app

--- news_project/results/views.py
+++ news_project/results/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 #from django import forms
 #from forms.forms as forms
 from django.http import HttpResponse
@@ -6,7 +7,17 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
 from forms.models import SearchHistory
 
+def createRequest(keyword, sortBy, category, language):
+    key = 'd46c2a4c9f2842c5bcf9b1948f61afbe'
 
+    url = ('https://newsapi.org/v2/everything?'
+              'q=' + keyword.replace(' ','+') + '&'
+              'sortBy=' + sortBy + '&'
+              'language=en&' 
+              'apiKey=' + key)
+
+    #response = requests.get(url)
+    return url
 
 
 @ensure_csrf_cookie
@@ -15,8 +26,10 @@ def resultPage(request):
     #return redirect("")
 
     latest = SearchHistory.objects.latest('id')
-    print(latest.keyword)
-    print(latest.sortBy)
-    print(latest.category)
-    print(latest.language)
+    # print(latest.keyword)
+    # print(latest.sortBy)
+    # print(latest.category)
+    # print(latest.language)
+    print(createRequest(latest.keyword, latest.sortBy, latest.category, latest.language))
+
     return render(request, 'results.html', {})
